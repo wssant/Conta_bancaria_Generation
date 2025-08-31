6d17c53bb1b37004684db55b5ae21c47201f7da9
Winnie <wssant13@gmail.com>
2025-08-31 18:05:42 -0300

    segundo commit

--- Menu.ts
+++ Menu.ts
@@ -1,1 +1,88 @@
-import 
+import readlinesync = require("readline-sync");
+
+export function main() {
+
+    let opcao: number;
+
+    while (true) {
+
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("                  CARPENTER BANK                     ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Criar Conta                          ");
+        console.log("            2 - Listar todas as Contas               ");
+        console.log("            3 - Buscar Conta por Numero              ");
+        console.log("            4 - Atualizar Dados da Conta             ");
+        console.log("            5 - Apagar Conta                         ");
+        console.log("            6 - Sacar                                ");
+        console.log("            7 - Depositar                            ");
+        console.log("            8 - Transferir valores entre Contas      ");
+        console.log("            9 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+
+        console.log("Entre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        if (opcao == 9) {
+            console.log("\nCarpenter Bank: Talhando oportunidades, lapidando conquistas.");
+            sobre();
+            process.exit(0);
+        }
+
+        switch (opcao) {
+            case 1:
+                console.log("\n\nCriar Conta\n\n");
+
+                break;
+            case 2:
+                console.log("\n\nListar todas as Contas\n\n");
+
+                break;
+            case 3:
+                console.log("\n\nConsultar dados da Conta - por número\n\n");
+
+                break;
+            case 4:
+                console.log("\n\nAtualizar dados da Conta\n\n");
+
+                break;
+            case 5:
+                console.log("\n\nApagar uma Conta\n\n");
+
+                break;
+            case 6:
+                console.log("\n\nSaque\n\n");
+
+                break;
+            case 7:
+                console.log("\n\nDepósito\n\n");
+
+                break;
+            case 8:
+                console.log("\n\nTransferência entre Contas\n\n");
+
+                break;
+            default:
+                console.log("\nOpção Inválida!\n");
+
+                break;
+        }
+    }
+
+}
+
+
+export function sobre(): void {
+    console.log("\n*****************************************************");
+    console.log("Projeto Desenvolvido por: Winnie Sant'Ana ");
+    console.log("Generation Brasil - generation@generation.org");
+    console.log("https://github.com/wssant/Conta_bancaria_Generation");
+    console.log("*****************************************************");
+}
+
+main();

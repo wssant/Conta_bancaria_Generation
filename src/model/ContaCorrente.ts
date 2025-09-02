7b38b29995553e86e61f5fdee44990dab6947e99
Winnie <wssant13@gmail.com>
2025-09-02 20:27:05 -0300

    quarto commit

---
+++ src/model/ContaCorrente.ts
@@ -0,0 +1,33 @@
+import { Conta } from "./Conta";
+
+export class ContaCorrente extends Conta{
+    private _limite: number
+
+    constructor(numero:number, agencia:number, tipo:number, titular:string, saldo:number, limite:number) {
+        super(numero, agencia, tipo, titular, saldo);
+        this._limite = limite;
+    }
+
+    public get limite() {
+        return this._limite;
+    }
+
+    public set limite (limite: number) {
+        this._limite = limite;
+    }
+
+    public sacar(valor:number): boolean {
+        if((this.saldo + this.limite) > valor) {
+            console.log("Saldo insuficiente.");
+            return false;
+        }
+
+        this.saldo = this.saldo - valor;
+        return true
+    }
+
+    public visualizar(): void {
+        super.visualizar();
+        console.log("Limite: " + this.limite.toFixed(2));
+    }
+}
---
+++ src/repository/ContaRepository.ts
@@ -0,0 +1,12 @@
+import { Conta }  from "../model/Conta"
+export interface ContaRepository {
+    procurarPorNumero(numero:number): void;
+    listarTodas(): void;
+    cadastrar(conta: Conta): void;
+    atualizar(conta: Conta): void;
+    deletar(conta: Conta): void;
+
+    sacar(numero:number, valor:number): void;
+    depositar(numero:number, valor:number): void;
+    transferir(numeroOrigem:number, numeroDestino:number, valor:number): void;
+}
